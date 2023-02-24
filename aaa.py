bc59476eeeeada04bcc25c0800a775d7315b245b
jungjieun <je_eun2735@naver.com>
2023-02-24 16:17:18 +0900

    merge branch

---
+++ aaa.py
@@ -0,0 +1,17 @@
+rubin = 1
+print(rubin)
+rubin = "바보"
+print(rubin)
+
+kdt = {
+  "삼백오호" : "홀쭉",
+  "공욱재" : "미남",
+
+}
+kdt_order = ["우리는","개발자","입니다"]
+
+print(kdt["공욱재"])
+print(kdt_order[0], kdt_order[1],kdt_order[2])
+
+for index in kdt_order:
+  print(index)
